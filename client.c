84f7465d1c4e2854d4165e28709b9c37548ece3e
LegendPhoenix66 <64265723+LegendPhoenix66@users.noreply.github.com>
2024-05-06 23:12:05 +0200

    sending the message as binary

--- client.c
+++ client.c
@@ -1,9 +1,38 @@
 #include "minitalk.h"
 
+// Send a message to the server
+void send_message(int pid, char *message)
+{
+	// Loop through each character in the message
+	while (*message)
+	{
+		// Loop through each bit in the character
+		for (int i = 7; i >= 0; i--)
+		{
+			// Send a SIGUSR1 signal if the bit is 1
+			if ((*message >> i) & 1)
+				kill(pid, SIGUSR2);
+			// Send a SIGUSR2 signal if the bit is 0
+			else
+				kill(pid, SIGUSR1);
+			// Wait for the server to confirm the signal
+			usleep(100);
+		}
+		// Move to the next character
+		message++;
+	}
+}
+
 int main(int argc, char **argv)
 {
 	argc = argc;
 	argv = argv;
+
+	// Get the PID of the server
+	int pid = ft_atoi(argv[1]);
+
+	// Send the message to the server
+	send_message(pid, argv[2]);
 
 	return 0;
 }
