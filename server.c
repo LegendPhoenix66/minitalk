88ea4a173a0ab12e4631ba3f17e3c39afc05e13f
LegendPhoenix66 <64265723+LegendPhoenix66@users.noreply.github.com>
2024-05-06 23:12:32 +0200

    receiving the binary and converting it back to characters

--- server.c
+++ server.c
@@ -1,12 +1,14 @@
 
 #include "minitalk.h"
 
+int g_received_bits = 0;
+
 void handle_sigusr1(int sig)
 {
     // This function will be called when the SIGUSR1 signal is received
     // Here, SIGUSR1 represents a binary 0
 	sig = sig;
-    ft_printf("Received binary 0\n");
+	g_received_bits = g_received_bits << 1;
 }
 
 void handle_sigusr2(int sig)
@@ -14,7 +16,7 @@ void handle_sigusr2(int sig)
     // This function will be called when the SIGUSR2 signal is received
     // Here, SIGUSR2 represents a binary 1
 	sig = sig;
-    ft_printf("Received binary 1\n");
+	g_received_bits = (g_received_bits << 1) | 1;
 }
 
 int main()
@@ -24,9 +26,15 @@ int main()
     signal(SIGUSR2, handle_sigusr2);
 
     ft_printf("PID: %d\n", getpid());
+	int count = 0;
     while(1){
         // wait for signal
         pause();
+		count++;
+		if (count % 8 == 0){
+			ft_printf("%c", g_received_bits);
+			g_received_bits = 0;
+		}
     }
     return 0;
 }
